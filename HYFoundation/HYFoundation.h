180ae6ee1e84e7ac8be989091717d57c9a5f87ec
HoyeonKim <allen.hoy@estsoft.com>
2022-06-21 16:58:53 +0900

    HYFoundation Init

---
+++ HYFoundation/HYFoundation.h
@@ -0,0 +1,18 @@
+//
+//  HYFoundation.h
+//  HYFoundation
+//
+//  Created by 김호연 on 2022/06/21.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for HYFoundation.
+FOUNDATION_EXPORT double HYFoundationVersionNumber;
+
+//! Project version string for HYFoundation.
+FOUNDATION_EXPORT const unsigned char HYFoundationVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <HYFoundation/PublicHeader.h>
+
+
